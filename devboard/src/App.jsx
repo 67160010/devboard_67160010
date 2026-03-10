203c43dd5329e5cfccdadcaa107f5788486da802
67160010 <67160010@go.buu.ac.th>
2026-03-10 20:16:13 +0700

    feat: add PostList

--- devboard/src/App.jsx
+++ devboard/src/App.jsx
@@ -1,6 +1,7 @@
 
 import Navbar from "./components/Navbar";
 import PostCard from "./components/PostCard";
+import PostList from "./components/PostList";
 
 const POSTS = [
   {
@@ -37,6 +38,7 @@ function App() {
     <div>
       <Navbar />
       <PostCard title="ทดสอบ" body="นี่คือเนื้อหาทดสอบ" />
+      <PostList posts={POSTS} />
     </div>
   );
 }
